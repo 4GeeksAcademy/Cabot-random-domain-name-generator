64d0ac807fa8b8f4cdb1f3010bff1b0fa92f204f
Jacob Cabot <166416094+JacobBCabot@users.noreply.github.com>
2024-05-16 17:55:32 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let button = document.querySelector("#generator");
+let htmlGeneratedDomain = document.querySelector("#domain-name");
+
+button.addEventListener("click", function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "taco", "raft"];
+  let extension = [".com", ".us", ".net", ".org", ".co"];
+
+  const randomPronoun = Math.floor(Math.random() * pronoun.length);
+  //console.log(pronoun[randomPronoun]);
+  const randomAdj = Math.floor(Math.random() * adj.length);
+  //console.log(adj[randomAdj]);
+  const randomNoun = Math.floor(Math.random() * noun.length);
+  //console.log(noun[randomNoun]);
+  const randomExtension = Math.floor(Math.random() * extension.length);
+  //console.log(extension[randomExtension]);
+  const randomDomain =
+    "www." +
+    pronoun[randomPronoun] +
+    adj[randomAdj] +
+    noun[randomNoun] +
+    extension[randomExtension];
+  //console.log(randomDomain);
+  htmlGeneratedDomain.innerHTML = randomDomain;
+});
